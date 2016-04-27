0e1a1b1b63db528775141e0847f3360378fb1f69
Danilo Vitoriano <vitoriano08@gmail.com>
2016-04-27 01:30:13 -0300

    add window.resize fn

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,18 @@
 /*AMenu aberto no desktop e fechado no mobile*/
 
 $(document).ready(function(){
+	$(window).resize(function() {
 		if(window.screen.width < 480) {
-		$("a[href$='#collapse1']").attr("aria-expanded","false");
-		$("a[href$='#collapse1']").addClass("collapsed");
-		$("div#collapse1").removeClass("in");
-		$("div#collapse1").addClass("collapse");
-	} else {
-		$("a[href$='#collapse1']").attr("aria-expanded","true");
-		$("a[href$='#collapse1']").removeClass("collapsed");
-		$("div#collapse1").addClass("in");
-		$("div#collapse1").removeClass("collapse");
+			$("a[href$='#collapse1']").attr("aria-expanded","false");
+			$("a[href$='#collapse1']").addClass("collapsed");
+			$("div#collapse1").removeClass("in");
+			$("div#collapse1").addClass("collapse");
+		} else {
+			$("a[href$='#collapse1']").attr("aria-expanded","true");
+			$("a[href$='#collapse1']").removeClass("collapsed");
+			$("div#collapse1").addClass("in");
+			$("div#collapse1").removeClass("collapse");
+		}
 	}
 //  if(window.screen.width< 1025){ $('#collapse1').removeClass('in'); }
 
