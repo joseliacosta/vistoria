105d80c2f421f1d0bfbd0017f556a8ba963d95bd
Danilo Vitoriano <vitoriano08@gmail.com>
2016-04-27 11:33:41 -0300

    update js on load

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,17 @@
 /*AMenu aberto no desktop e fechado no mobile*/
 
 $(document).ready(function(){
+	if(window.screen.width < 480) {
+			$("a[href$='#collapse1']").attr("aria-expanded","false");
+			$("a[href$='#collapse1']").addClass("collapsed");
+			$("div#collapse1").removeClass("in");
+			$("div#collapse1").addClass("collapse");
+		} else {
+			$("a[href$='#collapse1']").attr("aria-expanded","true");
+			$("a[href$='#collapse1']").removeClass("collapsed");
+			$("div#collapse1").addClass("in");
+			$("div#collapse1").removeClass("collapse");
+		}
 	$(window).resize(function() {
 		if(window.screen.width < 480) {
 			$("a[href$='#collapse1']").attr("aria-expanded","false");
